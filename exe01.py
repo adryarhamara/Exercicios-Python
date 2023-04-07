3a5457f823aa6adcc92e55f7d81ea13294bcc899
adryarhamara <heeyrhamara@gmail.com>
2023-04-06 22:46:21 -0300

    exercicio 01 - slide 2

--- exe01.py
+++ exe01.py
@@ -7,3 +7,13 @@ def valorDeX() :
     x = 43
     return x
 print(valorDeX())
+# uso de condição
+resposta = int(input("Digite uma opçaõ valida"))
+if resposta == 30 : print("Opção valida!")
+else : print("Opção invalida!")
+# vendo os tipos de variaveis
+print(type(1))
+print(type('Agatha'))
+print(type([1,2,'petalá']))
+print(type(True))
+print(type(8.790))
---
+++ slide2-exe03.py
@@ -0,0 +1,6 @@
+valorHoras = float(input("Digite o valor das horas "))
+horasTrabalhadas = float(input("Digite o valor das horas trabalhadas"))
+
+salario = valorHoras * horasTrabalhadas
+
+print(f"O valor das horas que você trabalhada {horasTrabalhadas:.2}\n o valor das horas que trabalha e {valorHoras}\n seu salario e {salario}R$")
