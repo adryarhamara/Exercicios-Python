caf082b9e5950a3387c568f09a44a76b5b3e653e
adryarhamara <heeyrhamara@gmail.com>
2023-04-06 22:58:04 -0300

    exercicios concluidos

---
+++ slide2/exe03.py
@@ -0,0 +1,6 @@
+#Converção de metros para centimetos
+print("Exercicio para converter metros para centrimetros")
+metros = int(input("Digite um numero para converter  "))
+centimetros = int(input("Digite outro numero "))
+
+print("Convertendo ficou", metros * centimetros)
